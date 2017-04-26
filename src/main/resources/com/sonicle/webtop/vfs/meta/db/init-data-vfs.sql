c9b0f01dc121897de55da84d35e73232cd513edf
Matteo Albinola <malbinola@sonicle.com>
2017-04-26 14:51:27 +0200

    Remove init data for settings

--- src/main/resources/com/sonicle/webtop/vfs/meta/db/init-data-vfs.sql
+++
@@ -1,6 +0,0 @@
-
--- ----------------------------
--- Records of settings
--- ----------------------------
-INSERT INTO "core"."settings" VALUES ('com.sonicle.webtop.vfs', 'mydocuments.uri', 'file:///sonicle/sonicle/webtop5/domains/{DOMAIN_ID}/{SERVICE_ID}/mydocuments/{USER_ID}/');
-
