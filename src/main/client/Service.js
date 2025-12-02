64b3e9feb860b477486cdb609161d53e945816c4
malbinola <malbinola@sonicle.com>
2025-12-02 16:58:35 +0100

    Use new wt-tool-scroll-inset CSS class

--- src/main/client/Service.js
+++ src/main/client/Service.js
@@ -138,7 +138,7 @@ Ext.define('Sonicle.webtop.vfs.Service', {
 					xtype: 'treepanel',
 					reference: 'trstores',
 					scrollable: { trackPosition: 'v' },
-					cls: 'wt-tool-tree wtvfs-tool-tree',
+					cls: 'wt-tool-tree wt-tool-scroll-inset wtvfs-tool-tree',
 					bodyCls: 'wt-tool-bg',
 					border: false,
 					useArrows: true,
